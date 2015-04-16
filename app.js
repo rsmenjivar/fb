f53cd5f689cd0766f9ec70957868083f9b2eed14
unknown <reynaldo.menjivar@7240-4Z25M12.smarshcorp.com>
2015-04-16 13:25:30 -0700

    FB Div

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 $(document).ready(function(){
 
 	var number = +prompt("Enter A Number");
+	var $fb = $("#fb");
 
  	if ( number % 1 != 0) {
  		alert("Only Whole Numbers Allowed")
  	}
 
  	else {
-	for (var i = 1; i <= number; i++) {
+		for (var i = 1; i <= number; i++) {
 
-		if ( ((i % 3) === 0) && (( i % 5 ) === 0) ) {
-  			$("#fb").append("fizzbuzz");
-		}
-		else if ( (i % 3) === 0) {
-  			$("#fb").append("buzz");
-		}
-		else if ( (i % 5) === 0 ) {
-  			$("#fb").append("fizz");
-		}
-		else {
-			$("#fb").append(i);
-		}
+			if ( ((i % 3) === 0) && (( i % 5 ) === 0) ) {
+	  			$fb.append("fizzbuzz");
+			}
+			else if ( (i % 3) === 0) {
+	  			$fb.append("buzz");
+			}
+			else if ( (i % 5) === 0 ) {
+	  			$fb.append("fizz");
+			}
+			else {
+				$fb.append(i);
+			}
 
-		$('#fb').append('<br />');
-	}
+			$fb.append('<br />');
+		}
 	}
 
 });
