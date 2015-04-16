11c357474bb96809c7bdf68344e33056d0d7331f
unknown <reynaldo.menjivar@7240-4Z25M12.smarshcorp.com>
2015-04-16 12:48:37 -0700

    Initial Commit

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-for (var i = 1; i <= 100; i++) {
-	/*console.log(i);*/
+$(document).ready(function(){
 
-	if ( ((i % 3) === 0) && (( i % 5 ) === 0) ) {
-  		console.log('FizzBuzz');
+	var number = +prompt("Enter A Number");
+
+ 	if ( number % 1 != 0) {
+ 		alert("Only Whole Numbers Allowed")
+ 	}
+
+ 	else {
+	for (var i = 1; i <= number; i++) {
+
+		if ( ((i % 3) === 0) && (( i % 5 ) === 0) ) {
+  			$("fizzbuzz").append("fizzbuzz");
 		}
-	else if ( (i % 3) === 0) {
-  		console.log('Fizz');
+		else if ( (i % 3) === 0) {
+  			$("fizzbuzz").append("buzz");
 		}
-	else if ( (i % 5) === 0 ) {
-  		console.log('Buzz');
+		else if ( (i % 5) === 0 ) {
+  			$("fizzbuzz").append("fizz");
 		}
-	else {
-		console.log(i);
+		else {
+			$("fizzbuzz").append(i);
 		}
 
-}
+		$('fizzbuzz').append('<br />');
+	}
+	}
+
+});
